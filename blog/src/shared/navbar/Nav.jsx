29f7fa296e795482463ddb4b88528e4cd3b75ea8
Mahamudul Hasan Mehedi <mahamudul.hasan.mehedi.01@gmail.com>
2026-06-25 01:29:48 +0600

    routing

--- blog/src/shared/navbar/Nav.jsx
+++ blog/src/shared/navbar/Nav.jsx
@@ -1,8 +1,11 @@
 "use client";
 import Link from 'next/link';
+import { usePathname, useRouter } from 'next/navigation';
 import React from 'react';
 
 const NavBar = () => {
+    const pathName=usePathname();
+    const router=useRouter()
     const nav_links = [
         {
             name: 'Home',   
@@ -11,11 +14,20 @@ const NavBar = () => {
         {
             name: 'About',
             link: '/about'
-        }
+        },
+        {
+            name: 'Contact',
+            link: '/contact'
+        },
+
     ];  
 
+    const handleSignIn = () => {
+        router.push('/login');
+    }
+
     return (
-        <nav className="sticky top-0 z-50 border-b border-white/10 bg-white/80 backdrop-blur-md">
+        <nav className="sticky top-0 z-50 shadow-2xl bg-blue-800 backdrop-blur-md">
             <div className="mx-auto flex w-full max-w-6xl items-center justify-between px-4 py-4 sm:px-6 lg:px-8">
                 <Link
                     href="/"
@@ -30,13 +42,21 @@ const NavBar = () => {
                             <Link
                                 key={item.link}
                                 href={item.link}
-                                className="rounded-full px-4 py-2 text-sm font-medium text-gray-600 transition hover:bg-white hover:text-gray-900 hover:shadow-sm"
+                                className={`rounded-full px-4 py-2 text-sm font-medium transition ${
+                                    pathName === item.link
+                                        ? 'bg-white text-gray-900 shadow-sm'
+                                        : 'text-gray-600 hover:bg-white hover:text-gray-900 hover:shadow-sm'
+                                }`}
                             >
                                 {item.name}
                             </Link>
                         );
                     })}
+                    
                 </div>
+                <div className="bg-white text-black px-2 py-1 rounded-full  text-semibold">
+                        <button onClick={handleSignIn}>Sign In</button>
+                    </div>
             </div>
         </nav>
     );
