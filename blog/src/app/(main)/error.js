7854bcba37a9daadf7d1c6d5c6f26effa60eabb2
Mahamudul Hasan Mehedi <mahamudul.hasan.mehedi.01@gmail.com>
2026-07-14 00:50:51 +0600

    error and loading page added

---
+++ blog/src/app/(main)/error.js
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react';
+
+const error = () => {
+    return (
+        
+            <h1 className="text-3xl font-bold text-center mt-10">Error occurred</h1>
+        
+    );
+};
+
+export default error;
---
+++ blog/src/app/(main)/loading.js
@@ -0,0 +1,10 @@
+'use client'
+import React from 'react';
+
+const loading = () => {
+    return (
+        <h1 className="text-3xl font-bold text-black text-center mt-10">Loading...</h1>
+    );
+};
+
+export default loading;
